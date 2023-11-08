b143cbdc74e3403ab4f3642609e5d66289ae6fa4
cindytrac <cindy.trac.11@gmail.com>
2023-11-08 16:30:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,22 +75,7 @@ with tab3:
        st.write(bytes_data)
        st.subheader("Acronyms Detected")
 
-   if uploaded_file is not None:
-       # To read file as bytes:
-       bytes_data = uploaded_file.getvalue()
-       st.write(bytes_data)
    
-       # To convert to a string based IO:
-       stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-       st.write(stringio)
-   
-       # To read file as string:
-       string_data = stringio.read()
-       st.write(string_data)
-   
-       # Can be used wherever a "file-like" object is accepted:
-       dataframe = pd.read_csv(uploaded_file)
-       st.write(dataframe)
       
 
 with tab4:
