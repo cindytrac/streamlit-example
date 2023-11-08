5af8a7328c70b3218924bc3a351ff81d3b4c239f
cindytrac <cindy.trac.11@gmail.com>
2023-11-08 15:55:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,24 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs(["Home", "Dog", "Owl"])
+tab1, tab2, tab3, tab4 = st.tabs(["Home", "Submit an Acronym", "Parse a Document", "About Project ACORN"])
 
 with tab1:
-   st.header("ERFT Dictionary")
-   title = st.text_input('Acronym', 'e.g. BofA, ERFT')
-   # st.write('Acronym', title)
-  
+   title = st.text_input('Input the ACRONYM you want to add.', 'e.g. BofA, ERFT')
+   
 
 with tab2:
-   st.header("A dog")
+   st.header("ERFT Dictionary")
+   
+   text_input = st.text_input(
+        "Enter the ACRONYM you would like to add",
+        label_visibility="visible",
+        placeholder="placeholder"
+    )
  
 with tab3:
    st.header("An owl")
+
+with tab4:
+   st.header("Project ACORN")
  
